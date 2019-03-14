d32572d8d637a7031c85932a280c60605cff1bed
Wroundy <roundywilliam97@gmail.com>
2019-03-14 10:25:54 -0600

    Updates

---
+++ precache-manifest.a92c8b0680c91327b36a9d7423532a05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "57ff912a099f11db8689",
+    "url": "/portfolio/static/css/main.4a277fb0.chunk.css"
+  },
+  {
+    "revision": "57ff912a099f11db8689",
+    "url": "/portfolio/static/js/main.57ff912a.chunk.js"
+  },
+  {
+    "revision": "f2aea4bb87df5695d018",
+    "url": "/portfolio/static/js/1.f2aea4bb.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "4f71f01ca114ced59cb7e0b5ed95daff",
+    "url": "/portfolio/index.html"
+  }
+];
